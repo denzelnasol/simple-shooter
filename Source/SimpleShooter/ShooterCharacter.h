94a61229544a79b80b371562e283da75882c41cf
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-09-10 16:30:42 -0700

    Dualshock 4 Controller aiming added

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -29,5 +29,10 @@ public:
 private:
 	void MoveForward(float AxisValue);
 	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 200;
 
 };
