2a23ebc7a748615a42b70d2717b2229e938f55b6
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-09-21 19:44:39 -0700

    Lose screen ui added

--- Source/SimpleShooter/ShooterPlayerController.h
+++ Source/SimpleShooter/ShooterPlayerController.h
@@ -18,6 +18,9 @@ public:
 	virtual void GameHasEnded(class AActor* EndGameFocus = nullptr, bool bIsWinner = false) override;
 
 private:
+	UPROPERTY(Editanywhere)
+	TSubclassOf<class UUserWidget> LoseScreenClass;
+
 	UPROPERTY(EditAnywhere)
 	float RestartDelay = 5;
 
