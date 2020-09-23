aa81f72ef70466eb7f76db624f10f1783978e8d5
Denzel Nasol <denzelnasol@users.noreply.github.com>
2020-09-22 21:52:26 -0700

    Add health bar

--- Source/SimpleShooter/ShooterCharacter.h
+++ Source/SimpleShooter/ShooterCharacter.h
@@ -24,6 +24,9 @@ protected:
 public:	
 	UFUNCTION(BlueprintPure)
 	bool IsDead() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetHealthPercent() const;
 	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
